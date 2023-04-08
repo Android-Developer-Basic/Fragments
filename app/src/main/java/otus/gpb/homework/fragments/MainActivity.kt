3684351307abdfa3063bfab190aea3795c70ffdd
Pavel Iohin <pavel@iohin.ru>
2023-04-08 22:28:02 +0300

    task 1

--- app/src/main/java/otus/gpb/homework/fragments/MainActivity.kt
+++ app/src/main/java/otus/gpb/homework/fragments/MainActivity.kt
@@ -1,6 +1,9 @@
 package otus.gpb.homework.fragments
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +11,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FragmentA.newInstance())
+                .addToBackStack(null)
+                .commit()
+        }
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    supportFragmentManager.popBackStack()
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
     }
 }
