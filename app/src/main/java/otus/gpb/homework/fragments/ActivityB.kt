eddcf24ee40db6c3a88f94bedebdc8ba76297917
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2023-12-12 13:19:18 +0300

    fixed device rotation issue

--- app/src/main/java/otus/gpb/homework/fragments/ActivityB.kt
+++ app/src/main/java/otus/gpb/homework/fragments/ActivityB.kt
@@ -2,6 +2,7 @@ package otus.gpb.homework.fragments
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +13,23 @@ class ActivityB : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_b)
+        findViewById<View>(R.id.fragmentContainerB)?.apply {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(id, FragmentBa(), FragmentBa::class.java.simpleName)
+                .commit()
+        }
+        findViewById<View>(R.id.fragmentContainerBa)?.apply {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(id, FragmentBa(), FragmentBa::class.java.simpleName)
+                .commit()
+        }
+        findViewById<View>(R.id.fragmentContainerBb)?.apply {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(id, FragmentBb(), FragmentBb::class.java.simpleName)
+                .commit()
+        }
     }
 }
