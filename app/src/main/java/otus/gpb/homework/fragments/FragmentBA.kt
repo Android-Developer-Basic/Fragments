a75e24dd7adbd9f309dd91f4482ac1c0cd4ed546
Andrusov <paandrusov@gid.ru>
2024-10-20 12:33:31 +0300

    Выполнено ДЗ Fragments

--- app/src/main/java/otus/gpb/homework/fragments/FragmentBA.kt
+++ app/src/main/java/otus/gpb/homework/fragments/FragmentBA.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import java.util.Objects
 
@@ -18,11 +17,6 @@ class FragmentBA : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         val view = inflater.inflate(R.layout.fragment_ba, container, false)
-
-        val textView = view.findViewById<TextView>(R.id.colorTextView)
-        parentFragmentManager.setFragmentResultListener("color", this) { _, bundle ->
-            textView.setBackgroundColor(bundle.getInt("color"))
-        }
 
         val openFragmentBbButton = view.findViewById<Button>(R.id.openFragmentBbButton)
         openFragmentBbButton.setOnClickListener {
